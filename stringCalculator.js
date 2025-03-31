5484c69b2e30ba73fab6cac91d1327dadfa873fa
lalitkumar-avataar <lalit.kumar@avataar.ai>
2025-03-31 22:48:01 +0530

    Feat: Add function supports newline as separator

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,6 @@
 function add(numbers) {
     if (!numbers) return 0; // Return 0 for empty string
-    return numbers.split(",").map(Number).reduce((sum, num) => sum + num, 0);
+    return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
