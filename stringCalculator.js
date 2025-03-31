1193d7b3b13d2327dbc754b813d72c40f4d7f97e
lalitkumar-avataar <lalit.kumar@avataar.ai>
2025-03-31 22:48:46 +0530

    Feat: Add function supports custom delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,15 @@
 function add(numbers) {
     if (!numbers) return 0; // Return 0 for empty string
-    return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const match = numbers.match(/^\/\/(.+)\n/);
+        if (match) {
+            delimiter = new RegExp(match[1]);
+            numbers = numbers.slice(match[0].length);
+        }
+    }
+
+    return numbers.split(delimiter).map(Number).reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
