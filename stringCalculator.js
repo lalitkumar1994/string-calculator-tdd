e2500234a7e5517e9e0b6f2acd91446720834b23
lalitkumar-avataar <lalit.kumar@avataar.ai>
2025-03-31 22:47:01 +0530

    Feat: Add function supports summing two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,6 @@
 function add(numbers) {
     if (!numbers) return 0; // Return 0 for empty string
-    return Number(numbers);
+    return numbers.split(",").map(Number).reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
