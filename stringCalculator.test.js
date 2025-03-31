df7b2b2c292cfbfff8a2c4ea8cf479843963dd5c
lalitkumar-avataar <lalit.kumar@avataar.ai>
2025-03-31 22:45:32 +0530

    Test: Add function returns number itself for a single input

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("./stringCalculator");
 test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("returns the number itself when one number is provided", () => {
+    expect(add("5")).toBe(5);
+});
