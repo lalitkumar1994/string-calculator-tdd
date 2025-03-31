3c4dfe357d7fb52fd50e905842ce488b370f49a6
lalitkumar-avataar <lalit.kumar@avataar.ai>
2025-03-31 22:47:43 +0530

    Test: Add function supports newline as a separator

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -11,3 +11,7 @@ test("returns the number itself when one number is provided", () => {
 test("returns sum of two numbers", () => {
     expect(add("1,2")).toBe(3);
 });
+
+test("handles new line as a separator", () => {
+    expect(add("1\n2,3")).toBe(6);
+});
